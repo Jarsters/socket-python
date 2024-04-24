3c5a21ce2dee0523542df976d4031183c050453d
Jarsters <kepalabotak9876@gmail.com>
2024-04-24 11:22:55 +0700

    feature: client-to-client connection, modul: model 1, message: percobaan pada LAN sudah berhasil

--- model 1/client.py
+++ model 1/client.py
@@ -1,21 +1,79 @@
 import socket
 import threading
+import json
+from typing import Any
+import time
 
-def listen_server(client):
+clients = []
+
+def listen_server(client, who="Server"):
     while True:
-        data = client.recv(1024).decode()
-        print(f"Server: {data}")
+        data = ""
+        try:
+            data = client.recv(1024)
+        except ConnectionAbortedError:
+            return "Done ga bang, doneeeee!"
+        if(not data):
+            return "Done ga bang, doneeeee!"
+        data = data.decode()
+        data = json.loads(data)
+        # print(f"Server: {data}")
+        print(f"{who}: {data}")
+
+def routine_message_to_connected_client():
+    global clients
+    while True:
+        for client in clients:
+            client.send(json.dumps("Halo ini pesan rutinan dari gue bro").encode())
+        time.sleep(10)
+
+def handle_another_client(socket_to_tracker: socket.socket):
+    socket_with_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_with_client.bind(socket_to_tracker.getsockname())
+    socket_with_client.listen()
+
+    threading.Thread(target=routine_message_to_connected_client, daemon=True).start()
+    
+    while True:
+        client_socket, _ = socket_with_client.accept() # _ = client_address
+        clients.append(client_socket)
+        print("Ada client baru konek")
+        threading.Thread(target=listen_server, args=(client_socket, "Client"), daemon=True).start()
 
 def send_message_to_server(client, message):
-    client.send(message.encode())
+    client.send(json.dumps(message).encode())
+
+def connecting_to_another_client():
+    socket_connecting_to_another_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sender = input("Masukkan nama pengirim: ")
+    address = input("Masukkan adress: ")
+    port = int(input("Masukkan port address: "))
+    socket_connecting_to_another_client.connect((address, port))
+
+    threading.Thread(target=listen_server, args=(socket_connecting_to_another_client, sender), daemon=True).start()
+    while True:
+        message = input("Pesan: ")
+        if(message.lower() == "exit"):
+            client.close()
+            break
+        send_message_to_server(socket_connecting_to_another_client, message)
+
 
 if __name__ == "__main__":
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     client.connect(("103.178.153.189", 55555))
+    # client.connect(("192.168.1.6", 55555))
+
+    username = input("Masukkan username: ")
+    client.send(f"username {username}".encode())
+
+    threading.Thread(target=handle_another_client, args=(client,), daemon=True).start()
     threading.Thread(target=listen_server, args=(client,), daemon=True).start()
     while True:
         message = input("Pesan: ")
         if(message.lower() == "exit"):
+            client.close()
             break
         send_message_to_server(client, message)
+    connecting_to_another_client()
--- model 1/server.py
+++ model 1/server.py
@@ -1,26 +1,56 @@
 import socket
 import threading
 import time
+import json
 
 clients = {}
 
-def handle_client(client_socket):
+# def handle_client(client_socket):
+#     while True:
+#         packet = client_socket.recv(1024).decode()
+#         if(packet.lower() == "halo server"):
+#             client_socket.send("Ada kebutuhan apa client?".encode())
+#         else:
+#             client_socket.send("Aku tidak mengerti dengan pesanmu!".encode())
+#         print(f"Mengirimkan pesan ke client {clients[client_socket]}")
+
+def handle_client(username):
+    client_socket = clients[username].get("socket")
     while True:
-        packet = client_socket.recv(1024).decode()
-        if(packet.lower() == "halo server"):
-            client_socket.send("Ada kebutuhan apa client?".encode())
-        else:
-            client_socket.send("Aku tidak mengerti dengan pesanmu!".encode())
-        print(f"Mengirimkan pesan ke client {clients[client_socket]}")
+        try:
+            packet = client_socket.recv(1024)
+            if(not packet):
+                del clients[username]
+                return "Done ga bang? Doneeee!"
+            packet = packet.decode()
+            packet = json.loads(packet)
+            if not packet:
+                del clients[username]
+                break
+            elif(packet.lower() == "halo server"):
+                client_socket.send(json.dumps("Ada kebutuhan apa client?").encode())
+            elif(packet.lower() == "daftar client"):
+                packet_result = json.dumps(get_format_client_address())
+                client_socket.send(packet_result.encode())
+            else:
+                client_socket.send(json.dumps("Aku tidak mengerti dengan pesanmu!").encode())
+            print(f"Mengirimkan pesan ke client {username}")
+        except:
+            del clients[username]
+            break
 
 def routine_message_from_server():
     global clients
     while True:
-        for client_socket in clients.keys():
-            client_socket.send("Halo ini pesan rutinan dari server".encode())
+        for client in clients.values():
+            client.get("socket").send(json.dumps("Halo ini pesan rutinan dari server").encode())
         time.sleep(10)
 
-threading.Thread(target=routine_message_from_server, daemon=True).start()
+def get_format_client_address():
+    result = []
+    for username, client in clients.items():
+        result.append((username, client.get("address")[0], client.get("address")[1]))
+    return result
 
 if __name__ == "__main__":
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -29,15 +59,26 @@ if __name__ == "__main__":
     server.listen()
     print("Server online...")
 
+    threading.Thread(target=routine_message_from_server, daemon=True).start()
+
     while True:
         try:
             client_socket, client_address = server.accept()
-            clients[client_socket] = client_address
-            print(f"Ada client baru terdaftar dari alamat {client_address}")
-            threading.Thread(target=handle_client, args=(client_socket,), daemon=True).start()
+            
+            username = client_socket.recv(1024).decode()
+            username = username.split("username ")[1]
+            print(f"Ada client baru terdaftar dari alamat {client_address} -- {username}")
+            # clients[client_socket] = client_address
+            clients[username] = {
+                "socket": client_socket,
+                "address": client_address
+            }
+            # threading.Thread(target=handle_client, args=(client_socket,), daemon=True).start()
+            threading.Thread(target=handle_client, args=(username,), daemon=True).start()
         except KeyboardInterrupt as err:
             print(err)
             break
         except Exception as err:
             print(err)
             break
+    print("==================AKHIR=====================")
