859bc8a07ac738ceeb07130eab6aaa55f11e18ad
Jarsters <kepalabotak9876@gmail.com>
2024-04-25 09:47:03 +0700

    bug-fixed: menghilangkan auto ping di server, add-feature: menambahkan kemampuan pada sisi client untuk bind port, modul: model 2, message: auto ping membuat server tidak mampu membalas packet client

--- model 2/client.py
+++ model 2/client.py
@@ -13,24 +13,44 @@ def opener_packet(packet):
 def wrapper_packet(packet, username):
     return (json.dumps(packet) + f" Pengirim@@ {username}").encode()
 
+def update_clients(clients):
+    global clients_key_username
+    for username, address in clients.items():
+        if(username != "Server"):
+            clients_key_username[username] = tuple(address)
+    # clients_key_username = clients
+    print(f"CKU: {clients_key_username}")
+
 def listener(socket: socket.socket):
     while True:
         message, address = socket.recvfrom(1024)
-        message, username = opener_packet(message)
         if not message:
             break
-        elif not message:
-            continue  
+        message, username = opener_packet(message)
+        if(type(message) == dict):
+            update_clients(message)
         else:
             if(not clients_key_address.get(address)):
                 clients_key_address[address] = username
                 clients_key_username[username] = address
             print(f"{username}: {message}")
 
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+    del s
+    return ip
+
 
 if __name__ == "__main__":
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    ip = get_ip()
+    port = int(input("Masukkan port untuk socket: "))
+    client_socket.bind(('0.0.0.0', port))
+    # client_socket.bind((ip, port))
 
     server_address = {
         "local": ('192.168.1.6', 55555),
@@ -56,9 +76,10 @@ if __name__ == "__main__":
     # client_socket.sendto("Halo server!".encode(), ('192.168.1.6', 55555))
     client_socket.sendto(wrapper_packet(f"Halo server! Aku {name}", name), server)
 
+    time.sleep(1) 
+
     threading.Thread(target=listener, args=(client_socket,), daemon=True).start()
 
-    time.sleep(1)
 
     while True:
         time.sleep(1)
@@ -69,13 +90,19 @@ if __name__ == "__main__":
         address_target = None
         if(not choose):
             target = input("Masukkan nama target: ")
-            address = input("Masukkan address target: ")
-            port = int(input("Masukkan port: "))
-            address_target = (address, port)
+            # address = input("Masukkan address target: ")
+            # port = int(input("Masukkan port: "))
+            address_target = clients_key_username[target]
+            f"{address_target} - {type(address_target)}"
+        elif(choose.lower() == "x"):
+            target = input("Masukkan username target: ")
+            address_target = tuple(clients_key_username[target])
         else:
             choose = int(choose)
             target = usernames[choose - 1]
-            address_target = clients_key_username[target]
+            address_target = tuple(clients_key_username[target])
+        print(f"Target: {target}")
+        print(f"Address target: {address_target}")
         packet = input("Masukkan pesan: ")
         if not packet:
             packet = wrapper_packet(f"Halo gua {name} salam kenal!", name)
--- model 2/server.py
+++ model 2/server.py
@@ -2,6 +2,7 @@ import socket
 import json
 import time
 import threading
+from pprint import pprint
 
 clients_key_address = {}
 clients_key_username = {}
@@ -26,9 +27,11 @@ def listener(socket: socket.socket):
         message, address = socket.recvfrom(1024)
         if not message:
             break
+        print(f"{address} - {type(address)}")
         message, username = opener_packet(message)
         if(message == "daftar client"):
             print(f"{username}: Meminta daftar client!")
+            pprint(clients_key_username)
             packet = wrapper_packet(clients_key_username, address) # Objek Daftar Client
             socket.sendto(packet, address)
         elif(message.lower().startswith("halo server!")):
@@ -43,33 +46,39 @@ def listener(socket: socket.socket):
             username_target, message = clear_message.split(" @@pesan@@ ")
             print(address)
             target = clients_key_username[username_target]
-            # sender = clients_key_address[address]
             packet = wrapper_packet(f"{message}", address)
             socket.sendto(packet, target)
         else:
             print(f"{username}: {message}")
 
-def ping_every_clients(socket: socket.socket):
-    while True:
-        sender_address = clients_key_username["Server"]
-        packet = wrapper_packet("", sender_address)
-        for client_target in clients_key_username.values():
-            socket.sendto(packet, client_target)
-        time.sleep(3)
+# def ping_every_clients(server_socket):
+#     # server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#     # server_socket.bind(('0.0.0.0', 55556))
+#     while True:
+#         sender_address = clients_key_username["Server"]
+#         packet = wrapper_packet("", sender_address)
+#         for client_target in clients_key_username.values():
+#             print(client_target)
+#             server_socket.sendto(packet, client_target)
+#         time.sleep(3)
 
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+    del s
+    return ip
 
 if __name__ == "__main__":
+    ip = get_ip()
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.bind(('0.0.0.0', 55555))
+    server_socket.bind((ip, 55555))
     address_server = server_socket.getsockname()
     clients_key_address[address_server] = "Server"
     clients_key_username["Server"] = address_server
 
-    threading.Thread(target=ping_every_clients, args=(server_socket,), daemon=True).start()
-
     print("Server online...")
-
-    # message, address = server_socket.recvfrom(1024)
 
     listener(server_socket)
     server_socket.close()
